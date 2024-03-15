180c437434546f9119ec33e9c34ca548cd1a854b
amaanmohd047 <amaanmohd047@gmail.com>
2024-03-15 16:15:32 +0530

    minor changes to social links

--- src/components/ui/Mail.tsx
+++ src/components/ui/Mail.tsx
@@ -1,30 +1,22 @@
 import React from "react";
+import FadeInWrapper from "./FadeInWrapper";
 
 const Mail: React.FC = () => {
   return (
     <>
-      <div className="fixed hidden sm:right-2 lg:right-5 left-auto bottom-0 sm:flex flex-col space-y-4 text-slate-light z-10 after:w-[1px] after:h-[8rem] after:bg-slate-light after:my-0 after:mx-auto after:block">
-        <div className="hover:text-green-regular transition-all duration-300 ease-in-expo cursor-pointer p-3 mb-5 tracking-wider hover:tracking-widest leading-5 hover:translate-y-[-2px]">
-          <a
-            href="mailto:amaanmohd047@gmail.com"
-            className="text-sm text-vertical-lr font-mono"
-            target="_blank"
-          >
-            amaanmohd047@gmail.com
-          </a>
+      <FadeInWrapper>
+        <div className="fixed hidden sm:right-2 lg:right-5 left-auto bottom-0 sm:flex flex-col space-y-4 text-slate-light z-10 after:w-[1px] after:h-[8rem] after:bg-slate-light after:my-0 after:mx-auto after:block">
+          <div className="hover:text-green-regular transition-all duration-300 ease-in-expo cursor-pointer p-3 mb-5 tracking-wider hover:tracking-widest leading-5 hover:translate-y-[-2px]">
+            <a
+              href="mailto:amaanmohd047@gmail.com"
+              className="text-sm text-vertical-lr font-mono"
+              target="_blank"
+            >
+              amaanmohd047@gmail.com
+            </a>
+          </div>
         </div>
-      </div>
-      {/* <div className="absolute sm:hidden flex justify-center w-[50%] max-w-[80%] bottom-0 text-slate-light">
-        <div className="hover:text-green-regular transition-all duration-300 ease-in-expo cursor-pointer p-3 mb-5 tracking-wider hover:tracking-widest leading-5 hover:translate-y-[-2px]">
-          <a
-            href="mailto:amaanmohd047@gmail.com"
-            className="font-mono"
-            target="_blank"
-          >
-            amaanmohd047@gmail.com
-          </a>
-        </div>
-      </div> */}
+      </FadeInWrapper>
     </>
   );
 };
--- src/components/ui/SocialLinks.tsx
+++ src/components/ui/SocialLinks.tsx
@@ -1,51 +1,64 @@
 import React from "react";
 import { FiInstagram, FiLinkedin, FiGithub } from "react-icons/fi";
+import FadeInWrapper from "./FadeInWrapper";
 
 const SocialLinks: React.FC = () => {
   return (
     <>
       {/* for normal screens */}
       <div className="fixed hidden lg:left-10 left-5 bottom-0 text-xl sm:flex flex-col space-y-3 text-slate-regular after:w-[1px] after:h-[7rem] after:bg-slate-light after:my-0 after:mx-auto after:block">
-        <div className="hover:text-green-regular transition-all w-auto h-auto duration-150 ease-in-expo font-mono hover:translate-y-[-2px] cursor-pointer py-3">
-          <a href="https://github.com/amaanmohd047" target="_blank">
-            <FiGithub />
-          </a>
-        </div>
-        <div className="hover:text-green-regular transition-all w-auto h-auto  duration-150 ease-in-expo font-mono hover:translate-y-[-2px] py-3 cursor-pointer">
-          <a href="https://www.linkedin.com/in/amaanmohd047/" target="_blank">
-            <FiLinkedin />
-          </a>
-        </div>
-        <div className="pb-9 hover:text-green-regular transition-all w-auto h-auto  duration-150 ease-in-expo hover:translate-y-[-2px] cursor-pointer py-3 font-mono">
-          <a
-            href="https://www.instagram.com/amaan_mohammed_047/"
-            target="_blank"
-          >
-            <FiInstagram />
-          </a>
-        </div>
+        <FadeInWrapper>
+          <div className="hover:text-green-regular transition-all w-auto h-auto duration-150 ease-in-expo font-mono hover:translate-y-[-2px] cursor-pointer py-3">
+            <a href="https://github.com/amaanmohd047" target="_blank">
+              <FiGithub />
+            </a>
+          </div>
+        </FadeInWrapper>
+        <FadeInWrapper>
+          <div className="hover:text-green-regular transition-all w-auto h-auto  duration-150 ease-in-expo font-mono hover:translate-y-[-2px] py-3 cursor-pointer">
+            <a href="https://www.linkedin.com/in/amaanmohd047/" target="_blank">
+              <FiLinkedin />
+            </a>
+          </div>
+        </FadeInWrapper>
+        <FadeInWrapper>
+          <div className="pb-9 hover:text-green-regular transition-all w-auto h-auto  duration-150 ease-in-expo hover:translate-y-[-2px] cursor-pointer py-3 font-mono">
+            <a
+              href="https://www.instagram.com/amaan_mohammed_047/"
+              target="_blank"
+            >
+              <FiInstagram />
+            </a>
+          </div>
+        </FadeInWrapper>
       </div>
 
       {/* for smaller screens */}
       <div className="flex sm:hidden absolute bottom-0 max-w-[60%] w-[40%] justify-between text-slate-regular text-xl">
-        <div className="hover:text-green-regular transition-all w-auto h-auto duration-150 ease-in-expo font-mono hover:translate-y-[-2px] cursor-pointer py-3">
-          <a href="https://github.com/amaanmohd047" target="_blank">
-            <FiGithub />
-          </a>
-        </div>
-        <div className="hover:text-green-regular transition-all w-auto h-auto  duration-150 ease-in-expo font-mono hover:translate-y-[-2px] py-3 cursor-pointer">
-          <a href="https://www.linkedin.com/in/amaanmohd047/" target="_blank">
-            <FiLinkedin />
-          </a>
-        </div>
-        <div className="pb-9 hover:text-green-regular transition-all w-auto h-auto  duration-150 ease-in-expo hover:translate-y-[-2px] cursor-pointer py-3 font-mono">
-          <a
-            href="https://www.instagram.com/amaan_mohammed_047/"
-            target="_blank"
-          >
-            <FiInstagram />
-          </a>
-        </div>
+        <FadeInWrapper>
+          <div className="hover:text-green-regular transition-all w-auto h-auto duration-150 ease-in-expo font-mono hover:translate-y-[-2px] cursor-pointer py-3">
+            <a href="https://github.com/amaanmohd047" target="_blank">
+              <FiGithub />
+            </a>
+          </div>
+        </FadeInWrapper>
+        <FadeInWrapper>
+          <div className="hover:text-green-regular transition-all w-auto h-auto  duration-150 ease-in-expo font-mono hover:translate-y-[-2px] py-3 cursor-pointer">
+            <a href="https://www.linkedin.com/in/amaanmohd047/" target="_blank">
+              <FiLinkedin />
+            </a>
+          </div>
+        </FadeInWrapper>
+        <FadeInWrapper>
+          <div className="pb-9 hover:text-green-regular transition-all w-auto h-auto  duration-150 ease-in-expo hover:translate-y-[-2px] cursor-pointer py-3 font-mono">
+            <a
+              href="https://www.instagram.com/amaan_mohammed_047/"
+              target="_blank"
+            >
+              <FiInstagram />
+            </a>
+          </div>
+        </FadeInWrapper>
       </div>
     </>
   );
