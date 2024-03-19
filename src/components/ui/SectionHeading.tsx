6c62dc021383af5da9fcdfa854aafc35bb00f04f
Amaan Mohammed <68446524+amaanmohd047@users.noreply.github.com>
2024-03-19 05:54:19 +0530

    Update SectionHeading.tsx
    reversed changes

--- src/components/ui/SectionHeading.tsx
+++ src/components/ui/SectionHeading.tsx
@@ -5,7 +5,7 @@ import { motion } from "framer-motion";
 const SectionHeading: FC<{ children: string }> = ({ children }) => {
   return (
     <motion.h1
-      className="uppercase py-1 md:py-3 lg:py-5 sm:pt-8 sm:pb-4 text-[1.4rem] sm:text-[2rem] lg:text-[2rem] text-green-dark duration-150 font-mono text-center font-semibold"
+      className=" py-1 md:py-3 lg:py-5 sm:pt-8 sm:pb-4 text-[1.4rem] sm:text-[2rem] lg:text-[2rem] text-green-dark duration-150 font-mono text-center font-semibold"
       variants={fadeInAnimation}
       whileInView="animate"
       initial="initial"
